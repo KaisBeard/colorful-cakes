5fa72e50d81f03a477c8b6011493e6aad10ed873
Kai <frenk.el.kai@gmail.com>
2021-11-11 14:17:20 +0100

    candles are flickering

--- hover-effect.js
+++ hover-effect.js
@@ -1,49 +1,110 @@
-let candleFlicker = document.getElementById("hover");
-let candles = document.getElementById("candle2");
+let candleFlicker1 = document.getElementById(`hover1`);
 
-const candleHoverEffect = (a) => {
-    if (a.classList.contains("off")) {
-        a.classList.remove("off");
-    } else { 
-        a.classList.add("off");
+ candleFlicker1.addEventListener("mouseover", () => {
+    const candle1a = document.getElementById(`candle1a`);
+    const candle1b = document.getElementById(`candle1b`);
+    
+    let candleHoverEffect = () => {
+        if (candle1a.classList.contains("off")) {
+            candle1a.classList.remove("off");
+            candle1b.classList.add("off");
+        } else { 
+            candle1a.classList.add("off");
+            candle1b.classList.remove("off");
+        }
     }
+    let flicker = setInterval(candleHoverEffect, 200);
+    candleFlicker1.addEventListener("mouseout", () => {clearInterval(flicker);}); 
+    //why do I need to do an arrow function on the line above?
+});
+
+
+let candleFlicker2 = document.getElementById(`hover2`);
+
+ candleFlicker2.addEventListener("mouseover", () => {
+    const candle1a = document.getElementById(`candle2a`);
+    const candle1b = document.getElementById(`candle2b`);
+    
+    let candleHoverEffect = () => {
+        if (candle1a.classList.contains("off")) {
+            candle1a.classList.remove("off");
+            candle1b.classList.add("off");
+        } else { 
+            candle1a.classList.add("off");
+            candle1b.classList.remove("off");
+        }
+    }
+    let flicker = setInterval(candleHoverEffect, 200);
+    candleFlicker2.addEventListener("mouseout", () => {clearInterval(flicker);}); 
+    //why do I need to do an arrow function on the line above?
+});
+
+let candleFlicker3 = document.getElementById(`hover3`);
+
+ candleFlicker3.addEventListener("mouseover", () => {
+    const candle1a = document.getElementById(`candle3a`);
+    const candle1b = document.getElementById(`candle3b`);
+    
+    let candleHoverEffect = () => {
+        if (candle1a.classList.contains("off")) {
+            candle1a.classList.remove("off");
+            candle1b.classList.add("off");
+        } else { 
+            candle1a.classList.add("off");
+            candle1b.classList.remove("off");
+        }
+    }
+    let flicker = setInterval(candleHoverEffect, 200);
+    candleFlicker3.addEventListener("mouseout", () => {clearInterval(flicker);}); 
+    //why do I need to do an arrow function on the line above?
+});
+
+let candleFlicker4 = document.getElementById(`hover4`);
+
+ candleFlicker4.addEventListener("mouseover", () => {
+    const candle1a = document.getElementById(`candle4a`);
+    const candle1b = document.getElementById(`candle4b`);
+    
+    let candleHoverEffect = () => {
+        if (candle1a.classList.contains("off")) {
+            candle1a.classList.remove("off");
+            candle1b.classList.add("off");
+        } else { 
+            candle1a.classList.add("off");
+            candle1b.classList.remove("off");
+        }
+    }
+    let flicker = setInterval(candleHoverEffect, 200);
+    candleFlicker4.addEventListener("mouseout", () => {clearInterval(flicker);}); 
+    //why do I need to do an arrow function on the line above?
+});
+
+
+
+
+
+/*
+let candleFunction = (hover1, candle1a, candle1b) =>
+{
+let candleFlicker = document.getElementById(`${hover1}`);
+candleFlicker.addEventListener("mouseover", (candle1a, candle1b) => {
+    const candle1a = document.getElementById(`${candle1a}`);
+    const candle1b = document.getElementById(`${candle1b}`);
+    
+    let candleHoverEffect = () => {
+        if (candle1a.classList.contains("off")) {
+            candle1a.classList.remove("off");
+            candle1b.classList.add("off");
+        } else { 
+            candle1a.classList.add("off");
+            candle1b.classList.remove("off");
+        }
+    }
+    let flicker = setInterval(candleHoverEffect, 200);
+    candleFlicker.addEventListener("mouseout", () => {clearInterval(flicker);}); 
+    //why do I need to do an arrow function on the line above?
+});
 }
 
-candleFlicker.addEventListener("mouseOver", candleHoverEffect(candles));
-
-/*
-    setInterval(candleHoverEffect()
-    , 500);*/
-
-
-/*
-
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
-
-
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-candleFlicker.addEventListener("mouseenter", function( event ) {
-    // highlight the mouseenter target
-    event.target.style.color = "purple";
-  
-    // reset the color after a short delay
-    setTimeout(function() {
-      event.target.style.color = "";
-    }, 500);
-  }, false);
-
-  function( event ) {
-  // highlight the mouseover target
-  event.target.style.color = "orange";
-
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
-  */
+candleFunction(hover1, candle1a, candle1b);
+*/
